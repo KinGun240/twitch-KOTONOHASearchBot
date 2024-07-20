4e94c26f250bcd1005712a6549312da77fc7fd26
近ガン <81510677+KinGun240@users.noreply.github.com>
2024-07-20 23:27:28 +0900

    Add files via upload

---
+++ v.1.0.0/config.py
@@ -0,0 +1,16 @@
+# 必須入力項目 ----------
+# メモしたoauth:~のパスワード
+Bot_OAUTH = 'oauth:**********'
+# Botのアカウント名
+Bot_ChannelName = '**********'
+# 実際に配信をするアカウント名
+Twitch_ChannelName = '**********'
+# この文字を作成したコマンドの頭に付けることでコマンドと認識してくれます
+BOT_PREFIX = '!'
+# CLIENT_ID = ''
+
+# その他オプション項目 ----------
+# Botのテキスト色…未実装
+# Blue, Coral, DodgerBlue, SpringGreen, YellowGreen, Green, OrangeRed, Red,
+# GoldenRod, HotPink, CadetBlue, SeaGreen, Chocolate, BlueViolet, and Firebrick
+TextColor = 'Green'
