f4492810c230daec034a5720eb16108f42b1df17
近ガン <81510677+KinGun240@users.noreply.github.com>
2024-07-21 04:12:34 +0900

    Add files via upload

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # 必須入力項目 ----------
 # メモしたoauth:~のパスワード
-Bot_OAUTH = 'oauth:ld8yi5ggpr74dcjcxyl7t0xn2a4ueg'
+Bot_OAUTH = 'oauth:**********'
 # Botのアカウント名
-Bot_ChannelName = 'kin_gun_bot'
+Bot_ChannelName = '**********'
 # 実際に配信をするアカウント名
-Twitch_ChannelName = 'kin_gun'
+Twitch_ChannelName = '**********'
 # この文字を作成したコマンドの頭に付けることでコマンドと認識してくれます
 BOT_PREFIX = '!'
 # CLIENT_ID = ''
 
 # その他オプション項目 ----------
-# Botのテキスト色
+# Botのテキスト色…未実装
 # Blue, Coral, DodgerBlue, SpringGreen, YellowGreen, Green, OrangeRed, Red,
 # GoldenRod, HotPink, CadetBlue, SeaGreen, Chocolate, BlueViolet, and Firebrick
-TextColor = 'HotPink'
+TextColor = 'Green'
